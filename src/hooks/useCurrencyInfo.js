3285c046dde7cb4f843b37edf8cfc999f1804341
krupa <krupa.patel@gmail.com>
2024-09-18 17:03:30 +0530

    completed functionality

--- src/hooks/useCurrencyInfo.js
+++ src/hooks/useCurrencyInfo.js
@@ -1,14 +1,14 @@
 import { useEffect, useState } from "react";
 
 function useCurrencyInfo(currency) {
-    const [data, setData] = ({})
+    const [data, setData] = useState({})
     useEffect(() => {
-        fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@2024-03-06/v1/currencies/{currency}.json`)
+        fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@2024-03-06/v1/currencies/${currency}.json`)
         .then((res) => res.json())
         .then((res) => setData(res[currency]))
-        console.log(data);
+        // console.log('data1',data);
     }, [currency]); 
-    console.log(data);
+    // console.log('data2',data);
     return data;
 }
 
